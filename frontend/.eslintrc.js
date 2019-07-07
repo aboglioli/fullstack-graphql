f298367db719d8d2b023458e72c528f485ac7dd1
Alan Boglioli <alan.boglioli@gmail.com>
2019-07-07 20:19:19 -0300

    add eslint for linting in frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    'react/jsx-uses-react': 1,
+    'react/jsx-no-undef': 2,
+    'react/jsx-wrap-multilines': 2,
+    'react/no-string-refs': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        'singleQuote': true,
+        'trailingComma': "all"
+      }
+    ],
+    'eqeqeq': ['error', 'always'],
+    'no-return-await': 2,
+    'require-await': 2,
+    'comma-dangle': ['error', 'always-multiline'],
+    'semi': ['error', 'always'],
+    'no-console': 0
+  },
+};
