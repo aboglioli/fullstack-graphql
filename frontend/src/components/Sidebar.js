ba97da001e58b493382b60ef08e1cea9f96ebcbe
Alan Boglioli <alan.boglioli@gmail.com>
2019-07-11 00:11:50 -0300

    [FRONTEND] sidebar sections can be hidden

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import './Sidebar.css';
 
-const Sidebar = ({ items, open, header, ...props }) => {
+const SidebarSection = ({ section, items }) => {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <div className="sidebar__section">
+      <h5 className="sidebar__section__title" onClick={() => setOpen(!open)}>{section}</h5>
+      <ul
+        className={`sidebar__section__items ${
+          open ? '' : 'sidebar__section__items--closed'
+        }`}
+      >
+        {items.map(({ link, text, ...rest }, i) => (
+          <li key={i} className="sidebar__section__item">
+            <a href={link} {...rest}>
+              {text}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+SidebarSection.propTypes = {
+  section: PropTypes.string.isRequired,
+  items: PropTypes.array.isRequired,
+};
+
+const Sidebar = ({ items, header, ...props }) => {
+  const [open, setOpen] = useState(true);
+
   return (
     <nav className={`sidebar ${open ? '' : 'sidebar--closed'}`} {...props}>
       <div className="sidebar__header">
         {header}
+        <button className="sidebar__button" onClick={() => setOpen(!open)}>
+          x
+        </button>
       </div>
       <div className="sidebar__content">
         {items.map((item, i) => (
-          <div key={i} className="sidebar__section">
-            <h5 className="sidebar__section__title">{item.section}</h5>
-            <ul className="sidebar__section__items">
-              {item.items.map(({ link, text, ...rest }, i) => (
-                <li key={i} className="sidebar__section__item">
-                  <a href={link} {...rest}>
-                    {text}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
+          <SidebarSection key={i} {...item} />
         ))}
       </div>
     </nav>
@@ -30,12 +52,7 @@ const Sidebar = ({ items, open, header, ...props }) => {
 
 Sidebar.propTypes = {
   items: PropTypes.array.isRequired,
-  open: PropTypes.bool,
   header: PropTypes.node,
-};
-
-Sidebar.defaultProps = {
-  open: true,
 };
 
 export default Sidebar;
